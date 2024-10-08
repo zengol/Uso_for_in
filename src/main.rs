859d225c358c5685513ebebdbfbbefc28d5631ba
davichogol <zengolutil@gmail.com>
2024-10-08 14:10:58 -0300

    uso del iterator for_in

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+
+// Uso del for_in 
+
+
+fn main() {
+
+   let matriz = [10, 20, 30, 40, 50, 60]; 
+    
+
+   for numero in matriz.iter() {
+
+    println!("el valor de la posicion es: {}", numero);
+
+   } 
+
+}
